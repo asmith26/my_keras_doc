c14cd055237b9521bfbb12054bb8dc0280d6683f
Andrew Smith <asmith26@users.noreply.github.com>
2016-05-01 20:19:41 +0100

    Add timestamps to ProgbarLogger.

--- keras/callbacks.py
+++ keras/callbacks.py
@@ -165,7 +165,7 @@ class ProgbarLogger(Callback):
 
     def on_epoch_begin(self, epoch, logs={}):
         if self.verbose:
-            print('Epoch %d/%d' % (epoch + 1, self.nb_epoch))
+            print(time.strftime("%Y%m%d-%H%M")+': Epoch %d/%d' % (epoch + 1, self.nb_epoch))
             self.progbar = Progbar(target=self.params['nb_sample'],
                                    verbose=self.verbose)
         self.seen = 0
